6a3cb796d2c8cc7698c889512d053dce394b6750
Canmi <9997200@qq.com>
2025-04-25 11:55:40 +0800

    mangr: chrome

--- browser.py
+++ browser.py
@@ -1,7 +1,13 @@
 import psutil
 import subprocess
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+
+options = Options()
+# options.add_argument("--headless")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 def is_chrome_running():
     for proc in psutil.process_iter(attrs=['pid', 'name']):
