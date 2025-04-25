7fb872f5fa9ef826cb3f56923d5454ee906fe3aa
Canmi <9997200@qq.com>
2025-04-25 12:01:24 +0800

    fix: checker

--- checker.py
+++ checker.py
@@ -1,5 +1,14 @@
 import re
 
-def is_lichess_game_tab(url):
-    pattern = r"^https://lichess.org/[A-Za-z0-9]{8}$"
-    return bool(re.match(pattern, url))
+class Checker:
+    def __init__(self, browser):
+        self.browser = browser
+        self.game_url_pattern = re.compile(r'^https://lichess\.org/[A-Za-z0-9]{8}$')
+
+    def check_for_game_url(self):
+        """Check all open tabs for a lichess game URL"""
+        tabs = self.browser.get_open_tabs()
+        for url in tabs:
+            if self.game_url_pattern.match(url):
+                return url
+        return None
